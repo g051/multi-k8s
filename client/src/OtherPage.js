71715682f1b5a362d0b4ff0cf490dbaa3e54afc4
g051 <68071451+g051@users.noreply.github.com>
2021-11-29 23:27:50 +0800

    ready in development environment

---
+++ client/src/OtherPage.js
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+
+const OtherPage = () => (
+    <div>
+        Im some other page!
+        <Link to="/">Go back home</Link>
+    </div>
+);
+
+export default OtherPage;
